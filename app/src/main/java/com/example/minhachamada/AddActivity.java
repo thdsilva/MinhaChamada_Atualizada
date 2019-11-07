72c47dbc7a56d309ef239e9d0be5e106a2b21c01
Thiago <thiagosilva9587@gmail.com>
2019-11-07 10:43:52 -0200

    adapters,controllers,helpers,entities adicionados
    Atual

--- app/src/main/java/com/example/minhachamada/AddActivity.java
+++ app/src/main/java/com/example/minhachamada/AddActivity.java
@@ -5,12 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.example.minhachamada.controllers.AlunoBancoController;
+import com.example.minhachamada.entities.Aluno;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 
 public class AddActivity extends AppCompatActivity {
 
@@ -49,4 +60,38 @@ public class AddActivity extends AppCompatActivity {
             imgContato.setImageBitmap(imageBitmap);
         }
     }
+    public void salvarDados(View view){
+        Bitmap bm=((BitmapDrawable)imgContato.getDrawable()).getBitmap();
+        String nomeArquivo = saveImageFile(bm);
+        Log.d("Arquivo Salvo",nomeArquivo);
+        Aluno aluno = new Aluno(0,ediName.getText().toString(),nomeArquivo);
+        if(new AlunoBancoController(this).add(aluno)){
+            Toast.makeText(this,"Aluno cadastrado",Toast.LENGTH_LONG).show();
+            finish();
+        }else{
+            Toast.makeText(this,"Erro ao Cadastrar Aluno",Toast.LENGTH_LONG).show();
+        }
+    }
+    public String saveImageFile(Bitmap bitmap) {
+        FileOutputStream out = null;
+        String filename = getFilename();
+        try {
+            out = new FileOutputStream(filename);
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, out);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return filename;
+    }
+    private String getFilename() {
+        File file = new File(Environment.getExternalStorageDirectory()
+                .getPath(), "TestFolder");
+        if (!file.exists()) {
+            file.mkdirs();
+        }
+        String uriString = (file.getAbsolutePath() + "/"
+                + System.currentTimeMillis() + ".jpg");
+        return uriString;
+    }
 }
+
--- app/src/main/java/com/example/minhachamada/MainActivity.java
+++ app/src/main/java/com/example/minhachamada/MainActivity.java
@@ -3,17 +3,25 @@ package com.example.minhachamada;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
+import com.example.minhachamada.adpters.AlunoAdapter;
+import com.example.minhachamada.controllers.AlunoBancoController;
+import com.example.minhachamada.entities.Aluno;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
     ListView lvAlunos;
     Button btnCadastrar;
+    ArrayList<Aluno> listaDeAlunos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,4 +53,19 @@ public class MainActivity extends AppCompatActivity {
         Intent it = new Intent(this, AddActivity.class);
         startActivity(it);
     }
+    public void atualizarLista(){
+        AlunoAdapter adapter = new AlunoAdapter(this, listaDeAlunos);
+        lvAlunos.setAdapter(adapter);
+        Cursor cursor = new AlunoBancoController(this).getAll();
+        listaDeAlunos.clear();
+        while (cursor.moveToNext()){
+            Aluno aluno = new Aluno(
+                    cursor.getInt(cursor.getColumnIndex("id")),
+                    cursor.getString(cursor.getColumnIndex("nome")),
+                    cursor.getString(cursor.getColumnIndex("foto")));
+            listaDeAlunos.add(aluno);
+        }
+        lvAlunos.deferNotifyDataSetChanged();
+    }
 }
+
